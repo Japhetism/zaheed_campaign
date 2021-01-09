c256a815202b4dd4448f3348da870572280970d8
babatunde.ojo <babatunde.ojo@Interswitchng.com>
2021-01-09 11:56:46 +0100

    revert phone number validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,15 +1,14 @@
 export function checkPhoneIsValid (phone) {
-    // if (phone.startsWith('+91') && phone.length === 13) {
-    //   return true
-    // }
-    // if (phone.startsWith('91') && phone.length === 12) {
-    //     return true
-    //   }
-    // if (phone.length === 10) {
-    //   return true
-    // }
-    //return false
-    return true
+    if (phone.startsWith('+91') && phone.length === 13) {
+      return true
+    }
+    if (phone.startsWith('91') && phone.length === 12) {
+        return true
+      }
+    if (phone.length === 10) {
+      return true
+    }
+    return false
 }
 
 export function checkPasswordIsValid (password) {
