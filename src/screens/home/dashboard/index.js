0d425f63e273bbf87dc82fe30a684be309048b25
babatunde.ojo <babatunde.ojo@Interswitchng.com>
2021-01-09 06:35:45 +0100

    add news carousel slider

--- src/screens/home/dashboard/index.js
+++ src/screens/home/dashboard/index.js
@@ -100,6 +100,49 @@ function Dashboard() {
                     </div>
                 </div>
             </div>
+            <div id="wrapper">
+                <div class="d-carousel">
+                    <ul class="carousel">
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt. </p>
+                        </li>
+                        <li> <a href="#"><img src="../../assets/css/news/style/images/carousel.jpg" alt="" class="img-news" /></a>
+                            <h4><a href="#">Cras varius, sapien egetory sagittis</a></h4>
+                            <p>Suspendisse potenti. Nibh miry, ultricies ac molestie sit ameter vestibulum eget nunc. Vivamus vitae aliquam odio. Morbi sit amet odio tincidunt.</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
         </div>                    
     </div>
   );
