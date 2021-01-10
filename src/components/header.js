23f7c643497ba2c259bd822c260c2ce538df0218
babatunde.ojo <babatunde.ojo@Interswitchng.com>
2021-01-10 10:08:52 +0100

    add notification

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import { retrieveStoredData } from '../utils/storage'
 
-function Header() {  
+function Header() {
+    const [formData, setFormData] = useState({
+        username: '',
+        notifications: []
+    })
+    const setHeaderInformation = () => {
+        const { username } = JSON.parse(retrieveStoredData('userInfo'))
+        const { person: { phoneVerified, points } } = JSON.parse(retrieveStoredData('userInfo'))
+        const notifications = []
+        if(!phoneVerified) {
+            notifications.push("Verify your phone number")
+        }
+        if(points <= 0) {
+            notifications.push("Complete your profile")
+        }
+        setFormData({...formData, username, notifications})
+    }
+    useEffect(() => {
+        setHeaderInformation()
+    }, [])  
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
@@ -13,7 +33,18 @@ function Header() {
                     <ul class="nav navbar-nav ml-auto">
                         <li class="nav-item">
                             <div class="dropdown">
-                                <span class="username">James Doe</span>
+                                <button class="btn btn-notification" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa fa-bell fa-lg"></i>
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <h5 class="dropdown-item align-item-center text-bold notification-text">Notifications</h5>
+                                    {formData.notifications.map(value => <li class="dropdown-item"><i class="fa fa-circle"></i>{`${value}.`}</li>)}
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <span class="username">{formData.username}</span>
                                 <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i class="fa fa-user fa-2x"></i>
                                 </button>
